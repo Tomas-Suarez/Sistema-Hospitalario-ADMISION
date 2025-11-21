2e9bebe0c51964708095b29cab667c14008e228d
Tomas Suarez <tomasa.suarez81@gmail.com>
2025-11-21 00:43:47 -0300

    fix(evaluacion): Correción en la vista y logica de la vista CargarEvaluacion

---
+++ public/js/Evaluacion.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("modal-editar");
+  const btnAbrir = document.getElementById("btn-abrir-modal");
+  const btnCerrar = document.getElementById("cerrar-modal");
+
+  if (btnAbrir) {
+    btnAbrir.addEventListener("click", () => {
+      modal.classList.remove("hidden");
+    });
+  }
+
+  if (btnCerrar) {
+    btnCerrar.addEventListener("click", () => {
+      modal.classList.add("hidden");
+    });
+  }
+
+  window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.classList.add("hidden");
+    }
+  });
+});
